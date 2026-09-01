d3f9cf09d469850bd548d8f35270597ca5fb23cc
ivanovandreidimitrov-ctrl <ivanovandreidimitrov@gmail.com>
2026-09-01 16:31:53 +0200

    v3.3: orb 80px, compressed header/main, chat area raised significantly

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lola Voice Service Worker
-const CACHE_NAME = 'lola-voice-v13';
+const CACHE_NAME = 'lola-voice-v14';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
