b7fd14585f87e93e20f0889bff40ad96f37ebaf9
ivanovandreidimitrov-ctrl <ivanovandreidimitrov@gmail.com>
2026-09-01 22:36:53 +0200

    v3.7: chat-log min-height 50vh, removed footer, force chat visible

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lola Voice Service Worker
-const CACHE_NAME = 'lola-voice-v17';
+const CACHE_NAME = 'lola-voice-v18';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
