412d3c95fb26b15b1ad2d9edd02f0feda0010302
ivanovandreidimitrov-ctrl <ivanovandreidimitrov@gmail.com>
2026-09-08 06:02:26 +0200

    v5.0: wake word A + MediaRecorder B + Groq Whisper + debug log + SW v22

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lola Voice Service Worker
-const CACHE_NAME = 'lola-voice-v21';
+const CACHE_NAME = 'lola-voice-v22';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
